fdf0f8ab8fb6d7e708f7a7e0458269fb9032236c
seokhyl <seokhyl@uci.edu>
2025-03-17 06:57:47 +0000

    modified ladder.cpp

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -84,13 +84,13 @@ void verify_word_ladder() {
     set<string> word_list;
 
     load_words(word_list, "./src/words.txt");
-    // my_assert(generate_word_ladder("cat", "dog", word_list).size() == 4);
-    // // cout << generate_word_ladder("cat", "dog", word_list).size() << endl;
-    // my_assert(generate_word_ladder("marty", "curls", word_list).size() == 6);
+    my_assert(generate_word_ladder("cat", "dog", word_list).size() == 4);
+    // cout << generate_word_ladder("cat", "dog", word_list).size() << endl;
+    my_assert(generate_word_ladder("marty", "curls", word_list).size() == 6);
     my_assert(generate_word_ladder("code", "data", word_list).size() == 6);
-    // my_assert(generate_word_ladder("work", "play", word_list).size() == 6);
-    // my_assert(generate_word_ladder("sleep", "awake", word_list).size() == 8);
-    // my_assert(generate_word_ladder("car", "cheat", word_list).size() == 4);
+    my_assert(generate_word_ladder("work", "play", word_list).size() == 6);
+    my_assert(generate_word_ladder("sleep", "awake", word_list).size() == 8);
+    my_assert(generate_word_ladder("car", "cheat", word_list).size() == 4);
 }
 
 
