a3719ecb112ffd042dab866a8a2b7d437d3ae21f
seokhyl <seokhyl@uci.edu>
2025-03-17 06:57:57 +0000

    modified ladder.cpp

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -1,33 +1,53 @@
 
 #include "ladder.h"
 
-using namespace std;
-
 void error(string word1, string word2, string msg) {
     cerr << "Error " << "(" << word1 << ", " << word2 << "): " << msg << endl;
 }
 
-bool edit_distance_within(const string& str1, const string& str2, int d) {
-    int str1Size = str1.size();
-    int str2Size = str2.size();
-    if (abs(str1Size - str2Size) > d) { return false; }
-
-    int diffCnt=0, i=0, j=0;
-    while(i<str1Size && j<str2Size) {
-        if(str1[i] != str2[j]) {
-            ++diffCnt;
-            if (diffCnt > d) { return false; }
-            if (str1Size > str2Size) ++i;
-            else if (str1Size > str2Size) ++j;
-            else { ++i; ++j; }
-        } else { ++i; ++j; }
+int calculate_edit_distance(const std::string& str1, const std::string& str2) {
+// https://en.wikipedia.org/wiki/Wagner%E2%80%93Fischer_algorithm
+    const int m = str1.size();
+    const int n = str2.size();
+    int d[50][50] = {0};
+    // int** d = new int[m+1][n+1];
+    
+    for (int i = 1; i <= m; ++i) {
+        d[i][0] = i;
     }
-    diffCnt += str1Size - i + str2Size - j;
-    return diffCnt <= d;
+    for (int j = 1; j <=n; ++j) {
+        d[0][j] = j;
+    }
+    int substitutionCost = 0;
+    for(int j=1; j<=n; ++j){
+        for(int i=1; i<=m; ++i){
+            substitutionCost = str1[i-1] == str2[j-1] ? 0 : 1;
+            
+            d[i][j] = min(min(d[i-1][j]+1, d[i][j-1]+1), d[i-1][j-1]+substitutionCost);
+        }
+    }
+    // for (int i=0; i<=m; ++i){
+    //     for(int j=0; j<=n; ++j) {
+    //         cout << d[i][j] << ' ';
+    //     }
+    //     cout << endl;
+    // }
+    return d[m][n];
+}
+
+bool edit_distance_within(const std::string& str1, const std::string& str2, int d) {
+    return calculate_edit_distance(str1, str2) <= d;
 }
 
 bool is_adjacent(const string& word1, const string& word2) {
     return edit_distance_within(word1, word2, 1);
+}
+
+template <typename T>
+void printVec(vector<T>& v) {
+    for (T e : v)
+        cout << e << ' ';
+    cout << endl;
 }
 
 vector<string> generate_word_ladder(const string& begin_word, const string& end_word, const set<string>& word_list) {
@@ -92,6 +112,3 @@ void verify_word_ladder() {
     my_assert(generate_word_ladder("sleep", "awake", word_list).size() == 8);
     my_assert(generate_word_ladder("car", "cheat", word_list).size() == 4);
 }
-
-
-
